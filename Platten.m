e8e80017489be86a3c44569f24f6f8ddc8ce89c3
Thomas Blum <thomasblum.work@gmail.com>
2019-01-24 18:12:57 +0100

    added platten geometry
    added platten geometry
    
    Added platten hole geometry for constructor, with local platten coordinates using the platten center as the origin.

--- Platten.m
+++ Platten.m
@@ -3,57 +3,71 @@ classdef Platten
     %   Detailed explanation goes here
     
     properties
-        
         type ; % cross or grid
         id ;   % id of the block
-       
-        xy_holes;
-             
         
+        xy_holes;   % positions of the transducer holes in local coordinates
     end
     
     
     properties (Dependent)
-         n_holes;
+        n_holes;
     end
-    
     
     methods
         
         % constructor
         function obj = Platten(ID) % ,varargin
             
-            obj.id=ID; % platten id
-            
+            obj.id = ID; % platten id
             
             switch ID
-                case {'A','B','C'}
+                case {'A','B'}
                     % it's a cross
                     obj.type='cross';
                     
-                    obj.xy_holes=[ 0 0  ; 
-                                   0.5 0.5;
-                                   1. 1.;
-                                   -0.5 0.5;
-                                   -1 1;];  % matrix nHoles times 2;
-
+                    % positions of holes on platen A and B in mm
+                    dcross = 18;    % top platen cross spacing
+                    ddiag1 = 34;    % top platen first diag spacing
+                    ddiag2 = 28;    % top platen additional diag spacing
+                    % compute positions
+                    x_tmp = [zeros(1,10), [5 4 3 2 1]*dcross, -[1 2 3 4 5]*dcross,...
+                        [-(ddiag1+2*ddiag2) -(ddiag1+ddiag2) -ddiag1 ddiag1 ddiag1+ddiag2,...
+                        ddiag1+2*ddiag2 -(ddiag1+2*ddiag2) -(ddiag1+ddiag2) -ddiag1 ddiag1,...
+                        ddiag1+ddiag2 ddiag1+2*ddiag2]/sqrt(2)]';
+                    y_tmp = [[5 4 3 2 1]*dcross, -[1 2 3 4 5]*dcross, zeros(1,10),...
+                        [(ddiag1+2*ddiag2) (ddiag1+ddiag2) ddiag1 ddiag1 ddiag1+ddiag2,...
+                        ddiag1+2*ddiag2 -(ddiag1+2*ddiag2) -(ddiag1+ddiag2) -ddiag1 -ddiag1,...
+                        -(ddiag1+ddiag2) -(ddiag1+2*ddiag2)]/sqrt(2)]';
+                    % write to property
+                    obj.xy_holes = [x_tmp, y_tmp];
                     
-                case {'D'}
+                    
+                case {'C','D','E','F'}
                     % it's a grid
-                    
                     obj.type='grid';
                     
-                    obj.xy_holes=[ 0 0 ];  % matrix nHoles times 2;
-                   
-                    
+                    % positions of holes on platen C, D, E and F in mm
+                    dhoriz = 50;    % side platen horiz spacing
+                    dvert1 = 20;    % side platen first vert spacing
+                    dvert2 = 30;    % side platen farther vert spacing
+                    % compute positions
+                    x_tmp = repmat([-dhoriz 0 dhoriz],1,4);
+                    y_tmp = [ones(1,3)*(dvert1+dvert2), ones(1,3)*dvert1,...
+                        ones(1,3)*-dvert1, ones(1,3)*-(dvert1+dvert2)];
+                    % write to property
+                    obj.xy_holes = [x_tmp, y_tmp];
             end
             
-%             % add varargin
-%             nVarargs = length(varargin);
-%             if (nVarargs==2)
-%                 obj.offset_x=varargin{1};
-%                 obj.offset_y=varargin{2};
-%             end
+            % get number of objects from length of coordinate property
+            obj.n_holes = length(obj.xy_holes);
+            
+            %             % add varargin
+            %             nVarargs = length(varargin);
+            %             if (nVarargs==2)
+            %                 obj.offset_x=varargin{1};
+            %                 obj.offset_y=varargin{2};
+            %             end
             
             
         end
